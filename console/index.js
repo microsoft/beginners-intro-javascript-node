b2715468c85f356f435faf875444e208ec57ed56
Burke Holland <burkeholland@gmail.com>
2020-09-21 16:54:05 -0500

    Add example code

---
+++ console/index.js
@@ -0,0 +1,11 @@
+const message = "Hello";
+const place = "World";
+
+// logging out a string
+console.log('Hello, World!');
+
+// using substitutions
+console.log('Hello, %s!', place);
+
+// using a string literal
+console.log(`${message}, ${place}!`);
