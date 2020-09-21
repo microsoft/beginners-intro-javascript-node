3850ccf81ee4997cb8c398d164a8eff53f7273e8
Jasmine <paladique@users.noreply.github.com>
2020-09-21 17:30:56 -0400

    add video demos

---
+++ Numbers_Demos/numbers_conversions.js
@@ -0,0 +1,30 @@
+let num1 = '150';
+let flo1 = '1.50';
+
+console.log("********Converting strings to integers********");
+//Converting strings to integers
+console.log(parseInt('100'));
+console.log(parseInt(num1));
+console.log(parseInt('ABC'));
+console.log(parseInt('0xF')); //Hexadecimal number
+
+console.log("********Converting strings to float********");
+//Converting strings to float
+console.log(parseFloat('1.00'));
+console.log(parseFloat(flo1));
+console.log(parseFloat('ABC'));
+
+console.log("********More Conversion Examples********");
+//More Conversion Examples
+//Numbers after special characters are ignored
+console.log(parseInt('1.5')); 
+console.log(parseInt('1 + 1'));
+
+//Using Template Literals
+console.log(parseInt(`${1 + 1}`));
+
+console.log("********Converting numbers to strings********");
+//Converting numbers to strings
+console.log(num1.toString());
+console.log(flo1.toString());
+console.log((100).toString());
---
+++ Strings_Demos/strings_templateliterals.js
@@ -0,0 +1,17 @@
+//Concatenation with template literals
+console.log("\n********Concatenation with template literals********\n");
+
+let str1 = "JavaScript";
+let str2 = "fun";
+
+console.log(`I am writing code in ${str1}`);
+console.log(`Formatting in ${str1} is ${str2}!`);
+
+//Expressions in template literals
+console.log("\n********Expressions in template literals********\n");
+
+let bool1 = true;
+console.log(`1 + 1 is ${1 + 1}`);
+console.log(`The opposite of true is ${!bool1}
+
+`)
